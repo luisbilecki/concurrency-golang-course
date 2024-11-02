496525c9d63905f580246e2f4c6c7584e65c45fd
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2024-11-01 21:49:36 -0300

    feat: add sync mutex to our code

--- mutex/main.go
+++ mutex/main.go
@@ -9,17 +9,22 @@ import (
 var msg string
 var wg sync.WaitGroup
 
-func updateMessage(s string) {
+func updateMessage(s string, m *sync.Mutex) {
 	defer wg.Done()
+
+	m.Lock()
 	msg = s
+	m.Unlock()
 }
 
 func main() {
 	msg = "Hello, world!"
 
+	var mu sync.Mutex
+
 	wg.Add(2)
-	go updateMessage("Hello, universe!")
-	go updateMessage("Hello, cosmos!")
+	go updateMessage("Hello, universe!", &mu)
+	go updateMessage("Hello, cosmos!", &mu)
 	wg.Wait()
 
 	fmt.Println(msg)
