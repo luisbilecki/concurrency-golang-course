203b6969fa7b70599e5626fd863cb6f004aaca6c
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2024-11-01 21:15:43 -0300

    feat: add challenge for wait group

---
+++ challenge-go-wg/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var msg string
+
+func updateMessage(s string) {
+	defer wg.Done()
+	msg = s
+}
+
+func printMessage() {
+	fmt.Println(msg)
+}
+
+var wg sync.WaitGroup
+
+func main() {
+	// challenge: modify this code so that the calls to updateMessage() on lines
+	// 28, 30, and 33 run as goroutines, and implement wait groups so that
+	// the program runs properly, and prints out three different messages.
+	// Then, write a test for all three functions in this program: updateMessage(),
+	// printMessage(), and main().
+
+	msg = "Hello, world!"
+
+	wg.Add(3)
+
+	updateMessage("Hello, universe!")
+	printMessage()
+
+	updateMessage("Hello, cosmos!")
+	printMessage()
+
+	updateMessage("Hello, world!")
+
+	printMessage()
+}
